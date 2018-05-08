5cb62fa68a55b4555bde9a0bc73eba7c6084aa63
blueskyxzy <blueskyxzy@gmail.com>
2018-05-08 15:34:01 +0800

    图灵机器人修改

--- mytest.py
+++ mytest.py
@@ -2,20 +2,28 @@
 # -*- coding: UTF-8 -*-
 
 from wxpy import *
-#
-
-
+from wxpy.contrib.tuling import Tuling
 robot = Robot()
 
-my_friend = robot.friends().search('谢力')[0]
+# my_friend = robot.friends().search('谢力')[0]
+#
+# my_friend.send('Hello WeChat!，我是小溪机器人')
 
-my_friend.send('Hello WeChat!，我是小溪机器人')
+# @robot.register(my_friend)
+# def reply_my_friend(msg):
+#     # 回复 my_friend 的消息 (优先匹配后注册的函数!)
+#     return 'received: {} ({})'.format(msg.text, msg.type)
+
+bot = Bot()
+my_friend = ensure_one(bot.search('谢力'))
+
+tuling = Tuling(api_key='43c87509771848f997cd48fba195b2be')
 
 
-@robot.register(my_friend)
+@bot.register(my_friend)
+# 使用图灵机器人自动与指定好友聊天
 def reply_my_friend(msg):
-    # 回复 my_friend 的消息 (优先匹配后注册的函数!)
-    return 'received: {} ({})'.format(msg.text, msg.type)
+    tuling.do_reply(msg)
 
 
 # 开始监听和自动处理消息
